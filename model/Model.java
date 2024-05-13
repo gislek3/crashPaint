7cc764c554c483ff5a82e7116e92e60c678f8a4c
contraposition <tumblrpod1@gmail.com>
2024-05-13 15:21:21 +0200

    step 2 done

--- model/Model.java
+++ model/Model.java
@@ -3,14 +3,27 @@ package model;
 import java.awt.Color;
 
 import model.grid.Grid;
+import model.grid.GridCell;
+import model.grid.GridDimension;
+import view.ViewableModel;
 
-public class Model extends Grid<Color> {
+public class Model extends Grid<Color> implements ViewableModel {
 
     ColorFactory colorFactory;
 
     public Model(int rows, int cols) {
         super(rows, cols);
         colorFactory = new RandomColorFactory();
+    }
+
+    @Override
+    public GridDimension getDimension() {
+        return this;
+    }
+
+    @Override
+    public Iterable<GridCell<Color>> getBoard() {
+        return this;
     }
     
 }
