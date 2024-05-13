9a44b59834ad9c17dea36490e6335e6d6e20df53
contraposition <tumblrpod1@gmail.com>
2024-05-13 16:33:13 +0200

    Finished

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import controller.Controller;
 import model.Model;
 import view.View;
 
 
 public class Main {
     public static void main(String[] args) {
-        Model model = new Model(9, 9);
+        Model model = new Model(64, 64);
         View view = new View(model);
+        new Controller(model, view);
         
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- model/Model.java
+++ model/Model.java
@@ -2,18 +2,25 @@ package model;
 
 import java.awt.Color;
 
+import controller.ControllableModel;
+import model.grid.CellPosition;
 import model.grid.Grid;
 import model.grid.GridCell;
 import model.grid.GridDimension;
 import view.ViewableModel;
 
-public class Model extends Grid<Color> implements ViewableModel {
+public class Model extends Grid<Color> implements ViewableModel, ControllableModel {
 
     ColorFactory colorFactory;
 
     public Model(int rows, int cols) {
         super(rows, cols);
         colorFactory = new RandomColorFactory();
+    }
+
+    @Override
+    public void set(CellPosition pos) {
+        this.set(pos, colorFactory.createColor());
     }
 
     @Override
