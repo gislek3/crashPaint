007c905cad76dd52b0b6bcb10991c8d81984d04a
contraposition <tumblrpod1@gmail.com>
2024-05-13 15:57:38 +0200

    step5 complete

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,5 +1,16 @@
 package controller;
 
-public class Controller {
-    
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Controller extends MouseAdapter {
+    private ControllableModel model;
+    private View view;
+
+    public Controller(ControllableModel model, View view) {
+        this.model = model;
+        this.view = view;
+        this.view.addMouseListener(this);
+    }
+
 }
