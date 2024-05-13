72749f5652cd62bd05160f059732e317e634ee8c
contraposition <tumblrpod1@gmail.com>
2024-05-13 15:58:27 +0200

    step5 complete

--- controller/Controller.java
+++ controller/Controller.java
@@ -3,6 +3,8 @@ package controller;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import view.View;
+
 public class Controller extends MouseAdapter {
     private ControllableModel model;
     private View view;
@@ -11,6 +13,12 @@ public class Controller extends MouseAdapter {
         this.model = model;
         this.view = view;
         this.view.addMouseListener(this);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        model.set(view.getPixelConverter().pointToCellPosition(e.getPoint()));
+        view.repaint();
     }
 
 }
