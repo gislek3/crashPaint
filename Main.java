0e08cfe259693378a5c6ad0119bf8c13aec10751
contraposition <tumblrpod1@gmail.com>
2024-05-13 15:10:47 +0200

    Updated main in step1

--- Main.java
+++ Main.java
@@ -1,19 +1,13 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import controller.Controller;
-import model.Model;
-import view.View;
 
 public class Main {
     public static void main(String[] args) {
-        Model model = new Model(9, 9);
-    
-        View view = new View(model);
-        new Controller(model, view);
         
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(view);
+        frame.setContentPane(new JPanel());
         frame.pack();
         frame.setVisible(true);
     }
